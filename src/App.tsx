f1bc63cadce11baaca74a24c23f40fab1ac1b76c
drogov <drogov@lanit.ru>
2024-06-03 23:09:54 +0300

    + menu burger

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import {slide as Menu} from 'react-burger-menu'
 import './App.css';
+import './menu.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import {Magicworld} from "./magicworld";
 
 function App() {
-  return (
-    <div className="App vh-100">
-      <Magicworld/>
-    </div>
-  );
+    return (
+        <>
+            <Menu pageWrapId='main'>
+                <a id="home" className="menu-item" href="/">Home</a>
+                <a id="about" className="menu-item" href="/about">About</a>
+                <a id="contact" className="menu-item" href="/contact">Contact</a>
+            </Menu>
+            <main id='main'>
+                <div className="App vh-100">
+                    <Magicworld/>
+                </div>
+            </main>
+        </>
+    );
 }
 
 export default App;
