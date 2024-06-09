ba59c174dd6ef056f28c18fcf95502f265712172
Dinuliat <dinuliat@mail.ru>
2024-06-09 14:34:07 +0300

    добавлены тени для фоновых картинок

--- src/safe-fishes.tsx
+++ src/safe-fishes.tsx
@@ -1,5 +1,9 @@
 export const SafeFishes = () => {
     return (
-        <div>Safe Fishes</div>
+        <div className="h-100 p-5">
+            <div className="MagicContainer h-100 w-100">
+                <div className="SafeFishesName">Безопасные рыбы</div>
+            </div>
+        </div>
     )
 }
