e668ba8aed6ffc5a8e65feb9caf19cc558787ffd
Dinuliat <dinuliat@mail.ru>
2024-07-01 21:32:29 +0300

    #fish-app4

--- src/dangerous-fishes.tsx
+++ src/dangerous-fishes.tsx
@@ -2,7 +2,7 @@ export const DangerousFishes = () => {
     return (
         <div className="h-100 p-5">
             <div className="MagicContainer h-100 w-100">
-                <div className="DangerousFishesName">Опасные рыбы</div>
+                <div className="DangerousFishesName">Опасные рыбы Красного моря</div>
             </div>
         </div>
     )
--- src/safe-fishes.tsx
+++ src/safe-fishes.tsx
@@ -2,7 +2,7 @@ export const SafeFishes = () => {
     return (
         <div className="h-100 p-5">
             <div className="MagicContainer h-100 w-100">
-                <div className="SafeFishesName">Безопасные рыбы</div>
+                <div className="SafeFishesName">Безопасные рыбы Красного моря</div>
             </div>
         </div>
     )
