becb91e9b6fe4efa4e6c69d56600aa562e38e1b5
Dinuliat <dinuliat@mail.ru>
2024-10-03 23:18:40 +0300

    optimized folders, added links to image to main pages, removed set page borders

--- src/pages/dangerous-fishes.tsx
+++ src/pages/dangerous-fishes.tsx
@@ -101,91 +101,115 @@ export const DangerousFishes = () => {
     }, [navigation])
 
     return (
-        <div className="h-100 p-5">
-            <div className="MagicContainer h-100 w-100">
+        <div className="pt-5 px-5">
+            <div className="MagicContainer">
                 <div className="FishesName">Опасные рыбы Красного моря</div>
                 <div className='Fishes-box'>
                     <div className='Box'>
                         <div onClick={onArototron} className='Arototron Fish'>
+                            <img src="image/dangerous-fishes/arototron/аротрон4.jpg" width="350" height="250" />
                             <p className="Fish_text">Арототрон</p>
                         </div>
                         <div onClick={onBaracuda} className='Baracuda Fish'>
+                            <img src="image/dangerous-fishes/baracuda/баракуда4.jpg" width="350" height="250" />
                             <p className="Fish_text">Баракуда</p>
                         </div>
                         <div onClick={onCrocodile} className='Crocodile Fish'>
+                            <img src="image/dangerous-fishes/crocodile/крокодил2.jpg" width="350" height="250" />
                             <p className="Fish_text">Рыба-крокодил</p>
                         </div>
                         <div onClick={onHammerfish} className='Hammerfish Fish'>
+                            <img src="image/dangerous-fishes/hammer-fish/рыба-молот4.jpg" width="350" height="250" />
                             <p className="Fish_text">Рыба-молот</p>
                         </div>
                     </div>
                     <div className='Box'>
                         <div onClick={onLionfish} className='Lionfish Fish'>
+                            <img src="image/dangerous-fishes/lionfish/крылатка3.jpg" width="350" height="250" />
                             <p className="Fish_text">Крылатка</p>
                         </div>
                         <div onClick={onLongwingedfish} className='Longwingedfish Fish'>
+                            <img src="image/dangerous-fishes/long-winged-fish/длиннокрылая акула1.jpeg" width="350" height="250" />
                             <p className="Fish_text">Длиннокрылая акула</p>
                         </div>
                         <div onClick={onMurena} className='Murena Fish'>
+                            <img src="image/dangerous-fishes/murena/мурена5.jpg" width="350" height="250" />
                             <p className="Fish_text">Мурена</p>
                         </div>
                         <div onClick={onSeasnake} className='Seasnake Fish'>
+                            <img src="image/dangerous-fishes/sea-snake/морская змея 1.jpg" width="350" height="250" />
                             <p className="Fish_text">Морская змея</p>
                         </div>
                     </div>
                     <div className='Box'>
                         <div onClick={onSharksblackfeather} className='Sharksblackfeather Fish'>
+                            <img src="image/dangerous-fishes/sharks-black-feather/Черноперая_рифовая_акула9.jpg" width="350" height="250" />
                             <p className="Fish_text">Рифовая акула</p>
                         </div>
                         <div onClick={onSharkscow} className='Sharkscow Fish'>
+                            <img src="image/dangerous-fishes/sharks-cow/бычья акула1.jpg" width="350" height="250" />
                             <p className="Fish_text">Бычья акула</p>
                         </div>
                         <div onClick={onSharksmaco} className='Sharksmaco Fish'>
+                            <img src="image/dangerous-fishes/sharks-maco/акула мако 4.jpg" width="350" height="250" />
                             <p className="Fish_text">Акула мако</p>
                         </div>
                         <div onClick={onSharksnuss} className='Sharksnuss Fish'>
+                            <img src="image/dangerous-fishes/sharks-nuss/акула нянька.jpg" width="350" height="250" />
                             <p className="Fish_text">Акула-нянька</p>
                         </div>
                     </div>
                     <div className='Box'>
                         <div onClick={onSharkswhite} className='Sharkswhite Fish'>
+                            <img src="image/dangerous-fishes/sharks-white/белая акула1.jpg" width="350" height="250" />
                             <p className="Fish_text">Белая акула</p>
                         </div>
                         <div onClick={onSharkszebra} className='Sharkszebra Fish'>
+                            <img src="image/dangerous-fishes/sharks-zebra/зебровая акула.jpg" width="350" height="250" />
                             <p className="Fish_text">Акула-зебра</p>
                         </div>
                         <div onClick={onTiggerfish} className='Tiggerfish Fish'>
+                            <img src="image/dangerous-fishes/tigger-fish/тигровая акула2.jpg" width="350" height="250" />
                             <p className="Fish_text">Тигровая акула</p>
                         </div>
                         <div onClick={onWhalesharks} className='Whalesharks Fish'>
+                            <img src="image/dangerous-fishes/whale-sharks/китовая акула6.jpg" width="350" height="250" />
                             <p className="Fish_text">Китовая акула</p>
                         </div>
                     </div>
                     <div className='Box'>
                         <div onClick={onStonefish} className='Stonefish Fish'>
+                            <img src="image/dangerous-fishes/stone-fish/рыба-камень 4.jpg" width="350" height="250" />
                             <p className="Fish_text">Рыба-камень</p>
                         </div>
                         <div onClick={onSkat} className='Skat Fish'>
+                            <img src="image/dangerous-fishes/skat/скат7.jpg" width="350" height="250" />
                             <p className="Fish_text">Скат-хвостокол</p>
                         </div>
                         <div onClick={onSkatorlyak} className='Skatorlyak Fish'>
+                            <img src="image/dangerous-fishes/skat/скат 12.jpg" width="350" height="250" />
                             <p className="Fish_text">Орляк</p>
                         </div>
                         <div onClick={onSkatmantu} className='Skatmantu Fish'>
+                            <img src="image/dangerous-fishes/skat/скат9.jpg" width="350" height="250" />
                             <p className="Fish_text">Манту</p>
                         </div>
                     </div>
                     <div className='Box'>
                         <div onClick={onTulozure} className='Tulozure Fish'>
+                            <img src="image/dangerous-fishes/tulozure/тилозур6.jpg" width="350" height="250" />
                             <p className="Fish_text">Тилозур</p>
                         </div>
                         <div onClick={onSurgeon} className='Surgeon Fish'>
+                            <img src="image/dangerous-fishes/surgeon/хирург7.jpg" width="350" height="250" />
                             <p className="Fish_text">Хирург</p>
                         </div>
                         <div onClick={onBalistod} className='Balistod Fish'>
+                            <img src="image/dangerous-fishes/spinorog/спинорог9.jpg" width="350" height="250" />
                             <p className="Fish_text">Балистод</p>
                         </div>
                         <div onClick={onSpinorog} className='Spinorog Fish'>
+                            <img src="image/dangerous-fishes/spinorog/спинорог17.jpg" width="350" height="250" />
                             <p className="Fish_text">Спинорог</p>
                         </div>
 
--- src/pages/safe-fishes.tsx
+++ src/pages/safe-fishes.tsx
@@ -82,77 +82,96 @@ export const SafeFishes = () => {
     }, [navigation])
 
     return (
-        <div className="h-100 p-5">
-            <div className="MagicContainer h-100 w-100">
+        <div className="p-5">
+            <div className="MagicContainer">
                 <div className="FishesName">Безопасные рыбы Красного моря</div>
 
                 <div className='Fishes-box'>
                     <div className='Box'>
                         <div onClick={onAngel} className='Angel Fish'>
+                            <img src="image/safe-fishes/angel/ангел10.jpg" width="350" height="250" />
                             <p className="Fish_text ">Рыба-ангел</p>
                         </div>
                         <div onClick={onAntias} className='Antias Fish'>
+                            <img src="image/safe-fishes/antias/антиас6.jpg" width="350" height="250" />
                             <p className="Fish_text ">Антиас</p>
                         </div>
                         <div onClick={onGrupper} className='Grupper Fish'>
+                            <img src="image/safe-fishes/gruppers/группер7.jpg" width="350" height="250" />
                             <p className="Fish_text ">Группер</p>
                         </div>
                         <div onClick={onDolphin} className='Dolphin Fish'>
+                            <img src="image/safe-fishes/dolphins/дельфин5.jpg" width="350" height="250" />
                             <p className="Fish_text ">Дельфин</p>
                         </div>
                     </div>
                     <div className='Box'>
                         <div onClick={onParrot} className='Parrot Fish'>
+                            <img src="image/safe-fishes/parrot/попугай9.jpg" width="350" height="250" />
                             <p className="Fish_text ">Рыба-попугай</p>
                         </div>
                         <div onClick={onTuna} className='Tuna Fish'>
+                            <img src="image/safe-fishes/tuna/тунец3.jpg" width="350" height="250" />
                             <p className="Fish_text ">Тунец</p>
                         </div>
                         <div onClick={onSolders} className='Soldiers Fish'>
+                            <img src="image/safe-fishes/soldiers/солдат1.jpg" width="350" height="250" />
                             <p className="Fish_text ">Солдатики</p>
                         </div>
                         <div onClick={onZebrosoma} className='Zebrosoma Fish'>
+                            <img src="image/safe-fishes/zebrosoma/зебрасомы2.jpg" width="350" height="250" />
                             <p className="Fish_text ">Зебрасома</p>
                         </div>
 
                     </div>
                     <div className='Box'>
                         <div onClick={onNemo} className='Nemo Fish'>
+                            <img src="image/safe-fishes/nemo/немо4.jpg" width="350" height="250" />
                             <p className="Fish_text">Рыба-клоун</p>
                         </div>
                         <div onClick={onCezii} className='Cezii Fish'>
+                            <img src="image/safe-fishes/cezii/цезии4.jpg" width="350" height="250" />
                             <p className="Fish_text">Цезии</p>
                         </div>
                         <div onClick={onKuzovok} className='Kuzovok Fish'>
+                            <img src="image/safe-fishes/kuzovok/кузовок4.jpg" width="350" height="250" />
                             <p className="Fish_text">Кузовок</p>
                         </div>
                         <div onClick={onFlute} className='Flute Fish'>
+                            <img src="image/safe-fishes/flute/дудочка14.jpg" width="350" height="250" />
                             <p className="Fish_text">Рыба-дудочка</p>
                         </div>
 
                     </div>
                     <div className='Box'>
                         <div onClick={onDugon} className='Dugon Fish'>
+                            <img src="image/safe-fishes/dugon/дюгонь5.jpg" width="350" height="250" />
                             <p className="Fish_text">Дюгонь</p>
                         </div>
                         <div onClick={onPlatex} className='Platex Fish'>
+                            <img src="image/safe-fishes/platex/платекс2.jpg" width="350" height="250" />
                             <p className="Fish_text ">Платексы</p>
                         </div>
                         <div onClick={onWhisperers} className='Whisperers Fish'>
+                            <img src="image/safe-fishes/whisperers/шептуны2.jpg" width="350" height="250" />
                             <p className="Fish_text">Шептуны</p>
                         </div>
                         <div onClick={onScolopsis} className='Scolopsis Fish'>
+                            <img src="image/safe-fishes/scolopsis/Scolopsis-ghanam2.jpg" width="350" height="250" />
                             <p className="Fish_text">Сколопсис</p>
                         </div>
                     </div>
                     <div className='Box'>
                         <div onClick={onNapoleon} className='Napoleon Fish'>
+                            <img src="image/safe-fishes/napoleons/наполеон7.jpg" width="350" height="250" />
                             <p className="Fish_text">Наполеон</p>
                         </div>
                         <div onClick={onLipsfish} className='Lipsfish Fish'>
+                            <img src="image/safe-fishes/lips-fish/губан2.jpg" width="350" height="250" />
                             <p className="Fish_text">Губаны</p>
                         </div>
                         <div onClick={onKabuba} className='Kabuba Fish'>
+                            <img src="image/safe-fishes/kabubas/кабубы2.jpg" width="350" height="250" />
                             <p className="Fish_text">Кабубы</p>
                         </div>
                     </div>
