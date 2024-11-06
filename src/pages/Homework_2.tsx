b0c9b556e6cbe8700488629ef0f33c4363a658da
Dinuliat <dinuliat@mail.ru>
2024-11-06 17:15:56 +0300

    add Homework2

--- src/pages/Homework_2.tsx
+++ src/pages/Homework_2.tsx
@@ -1,4 +1,16 @@
 export const Homework2 = () => {
-    return <>дз2
-    </>
+    return (
+        <div className="Pages text ">
+            <div className="Page">
+                <div className="Header"> Заголовок
+                </div>
+                <div className="Body"> Тело
+                </div>
+                <div className="Futer"> Подвал
+                </div>
+            </div>
+            <div className="Menu1"> Меню
+            </div>
+        </div>
+    )
 }
