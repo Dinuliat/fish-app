92a8b4818fa6a022958adf379dd683635242e297
Dinuliat <dinuliat@mail.ru>
2024-11-07 23:43:41 +0300

    task3,4,5 is done

--- src/pages/Homework_2.tsx
+++ src/pages/Homework_2.tsx
@@ -2,23 +2,27 @@ export const Homework2 = () => {
     return (
         <div className="Pages">
             <div className="Page">
-                <div className="Header"> 
+                <div className="Header">
                     <div className="cat"></div>
                     <div className="text">Усатые чемпионы</div>
                 </div>
                 <div className="Body">
                     <div className="text">
                         Котики - это космические пришельцы
-                                     
-
                     </div>
                 </div>
-                <div className="Futer"> 
+                <div className="Futer">
                     <div className="text">Шерстяные контакты</div>
                 </div>
             </div>
-            <div className="Menu1"> 
+            <div className="Menu1">
                 <div className="text">Меню</div>
+                <div className="Line">~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~</div>
+                <ul>
+                    <li>Наши пушистые проекты</li>
+                    <li>Наша усатая команда</li>
+                    <li>Шерстяные контакты</li>
+                </ul>
             </div>
         </div>
     )
---
+++ src/task4.js
@@ -0,0 +1,5 @@
+function lastValue(arr) {
+    console.log(arr[arr.length - 1]);
+}
+
+lastValue([1, 2, 3, 4, 5]);
